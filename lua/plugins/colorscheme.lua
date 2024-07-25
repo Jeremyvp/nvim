7caa5f23512c3c58bb8431a2a59c7a0e4653f0ea
Jeremy van Putten <Jeremyvp@users.noreply.github.com>
2024-07-26 01:42:03 +0200

    updated colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_autocmd('LspTokenUpdate', {
+  callback = function(args)
+    local token = args.data.token
+    if (token.type == 'type' or token.type == 'class') and token.modifiers.deduced then
+      vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, '@lsp.typemod.class.deduced')
+    elseif token.type == 'function' and token.modifiers.globalScope then
+      vim.lsp.semantic_tokens.highlight_token(token, args.buf, args.data.client_id, '@lsp.typemod.function.globalScope')
+    end
+  end,
+})
+
 return {
   'EdenEast/nightfox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -22,7 +33,7 @@ return {
           yellow = { base = '#d7cd5c', bright = '#ebdc7a', dim = '#a6a11e' },
           green = { base = '#84bf62', bright = '#9aeb6c', dim = '#629e3f' },
           orange = { base = '#e0ad3f', bright = '#f7d152', dim = '#d16e1d' },
-          blue = { base = '#7092e0', bright = '#90bff5', dim = '#6d78f3' },
+          blue = { base = '#7092e0', bright = '#a1cfff', dim = '#6d78f3' },
           white = { base = '#dddddd', bright = '#ffffff', dim = '#bbbbbb' },
           sel0 = '#444444',
         },
@@ -31,30 +42,33 @@ return {
         carbonfox = {
           syntax = {
             type = 'green',
-            func = 'blue',
-            number = 'yellow',
+            func = 'blue.bright',
+            number = 'yellow.dim',
             const = 'blue',
             conditional = 'blue',
             keyword = 'blue',
             preproc = '#999999',
-            string = '#e8663a',
+            string = '#e39866',
             field = '#e17aeb',
           },
         },
       },
       groups = {
         all = {
-          ['@function.builtin'] = { fg = 'palette.blue' },
+          ['@function.builtin'] = { fg = 'palette.blue.bright' },
           ['@type.builtin'] = { fg = 'palette.blue' },
           ['@module'] = { fg = 'palette.white.dim' },
           ['@parameter'] = { fg = 'palette.white' },
           ['@lsp.type.method'] = { fg = 'palette.yellow.bright' },
+          ['@lsp.typemod.type.defaultLibrary'] = { fg = 'palette.green' },
+          ['@lsp.typemod.type.deduced'] = { link = 'keyword' },
           ['@lsp.typemod.class.defaultLibrary'] = { fg = 'palette.green' },
           ['@lsp.typemod.class.constructorOrDestructor'] = { link = '@lsp.type.method' },
           ['@lsp.typemod.class.deduced'] = { link = 'keyword' },
           ['@lsp.typemod.function.defaultLibrary'] = { link = '@lsp.type.method' },
           ['@lsp.typemod.function.globalScope'] = { link = '@function.builtin' },
           ['@keyword.directive.define'] = { link = 'preproc' },
+          ['@keyword.directive'] = { link = 'preproc' },
         },
       },
     }
